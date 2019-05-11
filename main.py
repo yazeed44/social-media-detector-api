26a341a12f947a3ea1138eeeeb8617db70a2fa3f
Yazeed Almuqwishi <yazeed.almuqwishi@gmail.com>
2019-05-11 03:55:03 -0600

    throw an exception if the user does not provide an input
    Signed-off-by: Yazeed Almuqwishi <g--56@hotmail.com>

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os.path
 import sys
 
-from telethon import TelegramClient
+from telethon import TelegramClient, sync
 from telethon.tl.functions.contacts import ImportContactsRequest
 from telethon.tl.types import InputPhoneContact
 
@@ -15,7 +15,8 @@ def check_telegram(phone_number_list):
         input_contact_list = []
         for phone_number in phone_number_list:
             input_contact_list.append(
-                InputPhoneContact(client_id=0, phone=phone_number.get_phone_number(), first_name=phone_number.get_phone_number(), last_name='cba'))
+                InputPhoneContact(client_id=0, phone=phone_number.get_phone_number(),
+                                  first_name=phone_number.get_phone_number(), last_name='cba'))
         client(ImportContactsRequest(input_contact_list))
         for phone_number in phone_number_list:
             try:
@@ -30,7 +31,11 @@ def check_telegram(phone_number_list):
 
 
 def create_phone_numbers(arguments):
-    #Currently the proccessing of numbers to objects is sensitive. Meaning that empty and invalid lines might get processed into an objects, which obviously shouldn't. Bear that in mind
+    # Currently the proccessing of numbers to objects is sensitive. Meaning that empty and invalid lines might get processed into an objects, which obviously shouldn't. Bear that in mind
+
+    if len(arguments) <= 1:
+        return []
+
     phone_number_list = []
     if os.path.isfile(arguments[1]):
         with open(arguments[1]) as file:
@@ -39,12 +44,13 @@ def create_phone_numbers(arguments):
     else:
         for raw_phone_number in arguments[1:]:
             phone_number_list.append(PhoneNumber(raw_phone_number.strip('\n')))
-    #TODO Check if list is empty and provide warning
+    # TODO Check if list is empty and provide warning
     return phone_number_list
 
 
 def main():
     phone_number_list = create_phone_numbers(sys.argv)
+    assert phone_number_list != [], "The input is empty"
     check_telegram(phone_number_list)
     for number in phone_number_list:
         print(number)
